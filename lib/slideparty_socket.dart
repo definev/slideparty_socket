255389eba0d9a27a29adae4d534861b3a78fa086
definev <daiduong.workmail@gmail.com>
2022-02-06 18:42:56 +0700

    chore: remove unuse method

--- lib/slideparty_socket.dart
+++ lib/slideparty_socket.dart
@@ -2,15 +2,8 @@ export 'src/common/model/player_colors.dart';
 export 'src/common/state/player_state.dart'
     hide $PlayerDataCopyWith, $PlayerStateCopyWith, MapPlayerStateConverter;
 export 'src/common/event/client_event.dart'
-    hide
-        $ClientEventCopyWith,
-        $SendActionCopyWith,
-        $SendBoardCopyWith,
-        $SendNameCopyWith;
+    hide $ClientEventCopyWith, $SendActionCopyWith, $SendBoardCopyWith;
 export 'src/common/state/server_state.dart'
-    hide
-        $ServerStateCopyWith,
-        $RoomDataCopyWith,
-        $WrongBoardSizeCopyWith;
+    hide $ServerStateCopyWith, $RoomDataCopyWith, $WrongBoardSizeCopyWith;
 export 'src/common/model/room_info.dart';
 export 'src/common/model/slideparty_actions.dart';
